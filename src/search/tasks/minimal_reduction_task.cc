73fc52ebac25a6d4372e5d300ea548b74badde2b
AschwandenTobias <tobias.aschwanden@stud.unibas.ch>
2026-08-18 14:29:24 +0200

    Finish try implementation of minimalReductionTask

--- src/search/tasks/minimal_reduction_task.cc
+++ src/search/tasks/minimal_reduction_task.cc
@@ -61,3 +61,170 @@ bool MinimalReductionTask::are_facts_mutex(
         return parent->are_facts_mutex(fact1, fact2);
     }
 }
+
+// Returns true for every second operator
+bool MinimalReductionTask::is_skip_operator(int index) const {
+    return index % 2 == 1;
+}
+
+int MinimalReductionTask::get_plan_position(int index) const {
+    return index / 2;
+}
+
+// Here begins the harder part. We only need all operators in the plan and one
+// skip operator operator per operator :)
+int MinimalReductionTask::get_num_operators() const {
+    return plan.size() * 2;
+}
+
+int MinimalReductionTask::get_operator_cost(int index, bool is_axiom) const {
+    if (is_axiom) {
+        return parent->get_operator_cost(index, true);
+    }
+
+    if (is_skip_operator(index)) {
+        return 0;
+    }
+
+    int plan_position = get_plan_position(index);
+    int original_operator_index = plan[plan_position].get_index();
+
+    return parent->get_operator_cost(original_operator_index, false);
+}
+
+string MinimalReductionTask::get_operator_name(int index, bool is_axiom) const {
+    if (is_axiom) {
+        return parent->get_operator_name(index, true);
+    }
+    if (is_skip_operator(index)) {
+        return "skip_" + to_string(get_plan_position(index));
+    } else {
+        int original_operator_index =
+            plan[get_plan_position(index)].get_index();
+        return parent->get_operator_name(original_operator_index, false);
+    }
+}
+
+int MinimalReductionTask::get_num_operator_preconditions(
+    int index, bool is_axiom) const {
+    if (is_axiom) {
+        return parent->get_num_operator_preconditions(index, true);
+    }
+    if (is_skip_operator(index)) {
+        return 1;
+    }
+
+    int original_operator_index = plan[get_plan_position(index)].get_index();
+
+    return parent->get_num_operator_preconditions(
+               original_operator_index, false) +
+           1;
+}
+
+FactPair MinimalReductionTask::get_operator_precondition(
+    int op_index, int fact_index, bool is_axiom) const {
+    if (is_axiom) {
+        return parent->get_operator_precondition(op_index, fact_index, true);
+    }
+
+    int plan_position = get_plan_position(op_index);
+
+    if (is_skip_operator(op_index)) {
+        return FactPair(position_variable, plan_position);
+    }
+
+    int original_operator_index = plan[plan_position].get_index();
+    int original_num_preconditions =
+        parent->get_num_operator_preconditions(original_operator_index, false);
+
+    if (fact_index < original_num_preconditions) {
+        return parent->get_operator_precondition(
+            original_operator_index, fact_index, false);
+    }
+    return FactPair(position_variable, plan_position);
+}
+
+int MinimalReductionTask::get_num_operator_effects(
+    int index, bool is_axiom) const {
+    if (is_axiom) {
+        return parent->get_num_operator_effects(index, true);
+    }
+    if (is_skip_operator(index)) {
+        return 1;
+    }
+
+    int original_operator_index = plan[get_plan_position(index)].get_index();
+
+    return parent->get_num_operator_effects(original_operator_index, false) + 1;
+}
+
+FactPair MinimalReductionTask::get_operator_effect(
+    int op_index, int fact_index, bool is_axiom) const {
+    if (is_axiom) {
+        return parent->get_operator_effect(op_index, fact_index, true);
+    }
+
+    int plan_position = get_plan_position(op_index);
+
+    if (is_skip_operator(op_index)) {
+        return FactPair(position_variable, plan_position + 1);
+    }
+
+    int original_operator_index = plan[plan_position].get_index();
+
+    int original_num_effects =
+        parent->get_num_operator_effects(original_operator_index, false);
+
+    if (fact_index < original_num_effects) {
+        return parent->get_operator_effect(
+            original_operator_index, fact_index, false);
+    }
+
+    return FactPair(position_variable, plan_position + 1);
+}
+
+int MinimalReductionTask::get_num_operator_effect_conditions(
+    int op_index, int effect_index, bool is_axiom) const {
+    if (is_axiom) {
+        return parent->get_num_operator_effect_conditions(
+            op_index, effect_index, true);
+    }
+
+    if (is_skip_operator(op_index)) {
+        return 0;
+    }
+
+    int original_operator_index = plan[get_plan_position(op_index)].get_index();
+
+    int original_num_effects =
+        parent->get_num_operator_effects(original_operator_index, false);
+
+    if (effect_index < original_num_effects) {
+        return parent->get_num_operator_effect_conditions(
+            original_operator_index, effect_index, false);
+    }
+
+    // Added pos := i + 1 effect is unconditional.
+    return 0;
+}
+
+FactPair MinimalReductionTask::get_operator_effect_condition(
+    int op_index, int effect_index, int condition_index, bool is_axiom) const {
+    if (is_axiom) {
+        return parent->get_operator_effect_condition(
+            op_index, effect_index, condition_index, true);
+    }
+
+    int original_operator_index = plan[get_plan_position(op_index)].get_index();
+
+    return parent->get_operator_effect_condition(
+        original_operator_index, effect_index, condition_index, false);
+}
+
+vector<int> MinimalReductionTask::get_initial_state_values() const {
+    vector<int> values = parent->get_initial_state_values();
+
+    values.push_back(0);
+
+    return values;
+}
--- src/search/tasks/minimal_reduction_task.h
+++ src/search/tasks/minimal_reduction_task.h
@@ -12,6 +12,8 @@ private:
     Plan plan;
     int position_variable;
 
+    bool is_skip_operator(int index) const;
+    int get_plan_position(int index) const;
 public:
     MinimalReductionTask(
         const std::shared_ptr<AbstractTask> &parent, const Plan &plan);
@@ -22,7 +24,25 @@ public:
     int get_variable_axiom_layer(int var) const override;
     int get_variable_default_axiom_value(int var) const override;
     std::string get_fact_name(const FactPair &fact) const override;
-    bool are_facts_mutex(const FactPair &fact1, const FactPair &fact2) const;
+    bool are_facts_mutex(
+        const FactPair &fact1, const FactPair &fact2) const override;
+    int get_num_operators() const override;
+    int get_operator_cost(int index, bool is_axiom) const override;
+    std::string get_operator_name(int index, bool is_axiom) const override;
+    int get_num_operator_preconditions(int index, bool is_axiom) const override;
+    FactPair get_operator_precondition(
+        int op_index, int fact_index, bool is_axiom) const override;
+    int get_num_operator_effects(int index, bool is_axiom) const override;
+    FactPair get_operator_effect(
+        int op_index, int fact_index, bool is_axiom) const override;
+    int get_num_operator_effect_conditions(
+        int op_index, int effect_index, bool is_axiom) const override;
+
+    FactPair get_operator_effect_condition(
+        int op_index, int effect_index, int condition_index,
+        bool is_axiom) const override;
+
+    std::vector<int> get_initial_state_values() const override;
 };
 
 #endif
